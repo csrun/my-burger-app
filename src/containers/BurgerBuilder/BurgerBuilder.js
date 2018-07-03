35ee769daa3f70ad05dc8097336311ad369839bb
csrun <10632800+csrun@users.noreply.github.com>
2018-07-04 00:13:10 +0200

    pre dynamic ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,23 @@ import Auxilliaire from '../../hoc/Auxilliaire';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {...}
+  // }
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  }
+
   render () {
     return (
       <Auxilliaire>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Auxilliaire>
     );
