48ddeeef5201794322a1803b4f1c28c41796a337
csrun <10632800+csrun@users.noreply.github.com>
2018-07-07 14:26:27 +0200

    add order price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -102,6 +102,7 @@ class BurgerBuilder extends Component {
           modalClosed={this.purchaseCancelHandler}>
           <OrderSummary
             ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
             purchaseCanceled={this.purchaseCancelHandler}
             purchaseContinue={this.purchaseContinueHandler} />
         </Modal>
